580d07036215cfd295b0559651119cbd5c0d21d6
Grace <huang.yahui.grace@gmail.com>
2022-01-01 11:50:50 +0800

    初始

---
+++ src/main/java/com/myyhhuang/springbootdemo1/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.myyhhuang.springbootdemo1;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Springbootdemo1Application.class);
+	}
+
+}
